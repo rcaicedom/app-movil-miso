8e5012fad48236a9c93770aff60548f1fef8e496
catorrese <ca.torrese@uniandes.edu.co>
2023-04-20 00:05:39 -0500

    Login activity inicializacion basica

--- Vinilos/app/src/main/java/com/example/vinilos/ui/login/LoginActivity.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/login/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.example.vinilos.ui.login
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.vinilos.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var usuarioEdit: EditText
+    private lateinit var contrasenaEdit: EditText
+    private lateinit var ingresarButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        usuarioEdit = binding.usuarioEdit
+        contrasenaEdit = binding.contrasenaEdit
+        ingresarButton = binding.ingresarButton
+        val loading = binding.loading
 
+        ingresarButton.setOnClickListener {
+            loading.visibility = View.VISIBLE
+        }
     }
 }
