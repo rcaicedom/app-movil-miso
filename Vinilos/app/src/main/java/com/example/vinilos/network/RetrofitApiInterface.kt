538816b9f44e6b4cd2035ec7793197297dbf4e87
catorrese <ca.torrese@uniandes.edu.co>
2023-04-24 00:07:06 -0500

    Albums repository, VM y http requests implementados

--- Vinilos/app/src/main/java/com/example/vinilos/network/RetrofitApiInterface.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/network/RetrofitApiInterface.kt
@@ -1,8 +1,12 @@
 package com.example.vinilos.network
 
+import com.example.vinilos.data.album.Album
 import retrofit2.Call
 import retrofit2.http.*
 
 interface RetrofitApiInterface {
+
+    @GET("/albums")
+    fun getAlbums(): Call<List<Album>>
 
 }
