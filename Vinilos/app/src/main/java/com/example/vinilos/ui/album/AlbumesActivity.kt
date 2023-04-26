e674e691f1dba29b1417415ca489d0e07a76ede2
catorrese <ca.torrese@uniandes.edu.co>
2023-04-25 22:11:22 -0500

    Mensaje de albumes vacios implementado

--- Vinilos/app/src/main/java/com/example/vinilos/ui/album/AlbumesActivity.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/ui/album/AlbumesActivity.kt
@@ -2,6 +2,7 @@ package com.example.vinilos.ui.album
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -18,11 +19,14 @@ class AlbumesActivity: AppCompatActivity() {
     private lateinit var viewModel: AlbumViewModel
     private lateinit var adapter: AlbumesAdapter
     private lateinit var recycler: RecyclerView
+    private lateinit var albumesVaciosText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         albumesBinding = ActivityAlbumesBinding.inflate(layoutInflater)
         setContentView(albumesBinding.root)
+        albumesVaciosText = albumesBinding.albumesVacioText
+        albumesVaciosText.visibility = View.GONE
         recycler = findViewById(R.id.recyclerAlbumes)
         adapter = AlbumesAdapter()
         recycler.layoutManager = GridLayoutManager(this,2)
@@ -30,8 +34,13 @@ class AlbumesActivity: AppCompatActivity() {
 
         viewModel = ViewModelProvider(this, AlbumViewModel.Factory(application)).get(AlbumViewModel::class.java)
         viewModel.albums.observe(this) {
-            recycler.visibility = View.VISIBLE
-            adapter.setData(it as ArrayList<Album>)
+            if(it.isNotEmpty()) {
+                albumesVaciosText.visibility = View.GONE
+                recycler.visibility = View.VISIBLE
+                adapter.setData(it as ArrayList<Album>)
+            }else{
+                albumesVaciosText.visibility = View.VISIBLE
+            }
         }
         viewModel.eventNetworkError.observe(this) { isNetworkError ->
             if (isNetworkError) onNetworkError()
