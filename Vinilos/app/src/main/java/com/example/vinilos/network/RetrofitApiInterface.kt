efea78ea60f7e117c963cb6494d1407c150d3340
rcaicedom <r.caicedom@uniandes.edu.co>
2023-04-26 00:21:11 -0500

    Feature/hu03 (#12)
    * Albumes view
    
    * Artists view
    
    * Artists view
    
    * Artists view
    
    * Fixes
    
    ---------
    
    Co-authored-by: catorrese <ca.torrese@uniandes.edu.co>

--- Vinilos/app/src/main/java/com/example/vinilos/network/RetrofitApiInterface.kt
+++ Vinilos/app/src/main/java/com/example/vinilos/network/RetrofitApiInterface.kt
@@ -1,6 +1,7 @@
 package com.example.vinilos.network
 
 import com.example.vinilos.data.album.Album
+import com.example.vinilos.data.artista.Artista
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -9,4 +10,6 @@ interface RetrofitApiInterface {
     @GET("/albums")
     fun getAlbums(): Call<List<Album>>
 
+    @GET("/musicians")
+    fun getArtists(): Call<List<Artista>>
 }
