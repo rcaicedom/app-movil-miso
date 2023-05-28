ee0e416f4756cfd1ce326cc054a3937d923b3cec
rcaicedom <r.caicedom@uniandes.edu.co>
2023-05-27 22:20:31 -0500

    Hu 7 y 8 E2E test

--- Vinilos/app/src/androidTest/java/com/example/vinilos/E2ETests.java
+++ Vinilos/app/src/androidTest/java/com/example/vinilos/E2ETests.java
@@ -2,16 +2,27 @@ package com.example.vinilos;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
 
 import android.view.View;
+import android.widget.DatePicker;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -20,10 +31,13 @@ import com.example.vinilos.ui.login.LoginActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Date;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -93,6 +107,46 @@ public class E2ETests {
         Thread.sleep(1000);
         onView(allOf((withId(R.id.albumDetalleImagen)))).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void hu7End2EndTest() throws InterruptedException {
+        ViewInteraction skipBtn = onView(allOf(withId(R.id.ingresarColeccionista),withText("Ingresar como Coleccionista"), isDisplayed()));
+        skipBtn.perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.agregarAlbumButton)).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(withId(R.id.titleCrear))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.editCrearNombre))).perform(typeText("album test"));
+        onView(allOf(withId(R.id.editCrearCover))).perform(typeText("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphoto%2Fnewborn-chick-gm466167557-33570558&psig=AOvVaw2BQuuQrKz9DRSysMsDNEUN&ust=1685298117202000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCODViu-Olv8CFQAAAAAdAAAAABAD"));
+        onView(allOf(withId(R.id.editCrearFecha))).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2023, 06, 27));
+        Thread.sleep(3000);
+        onView(anyOf(withText(android.R.string.ok))).inRoot(isDialog()).perform(click());
+        onView(allOf(withId(R.id.editCrearDescripcion))).perform(typeText("test description")).perform(closeSoftKeyboard());
+        onView(allOf(withId(R.id.spinnerCrearGenero))).perform(click());
+        onView(allOf(withText("Salsa"))).perform(click());
+        onView(allOf(withId(R.id.spinnerCrearCasa))).perform(click());
+        onView(allOf(withText("EMI"))).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(withId(R.id.buttonCrearAlbum))).perform(click());
+    }
+
+    @Test
+    public void hu8End2EndTest() throws InterruptedException {
+        ViewInteraction skipBtn = onView(allOf(withId(R.id.ingresarColeccionista),withText("Ingresar como Coleccionista"), isDisplayed()));
+        skipBtn.perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.recyclerAlbumes)).perform(RecyclerViewActions.actionOnItemAtPosition(6, click()));
+        Thread.sleep(1000);
+        onView(withId(R.id.buttonAsociarTrack)).perform(click());
+        onView(allOf(withId(R.id.titleAsociar))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.editAsociarNombre))).perform(typeText(" test track"));
+        onView(allOf(withId(R.id.editAsociarDuracion))).perform(typeText("500")).perform(closeSoftKeyboard());
+        onView(allOf(withId(R.id.buttonAsociar))).perform(click());
+    }
+
+
+
 
 
 
